d4c4e74604a444b6c2693c64ab2c3d1de7f02bf2
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-10-11 19:34:53 +0900

    :sparkles: 첫 번째 테스트: 모든 규칙을 충족하는 경우

---
+++ src/test/java/com/example/tddstart/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,19 @@
+package com.example.tddstart.chap02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class PasswordStrengthMeterTest {
+
+	@Test
+	void meetsAllCriteria_Then_String() {
+		PasswordStrengthMeter meter = new PasswordStrengthMeter();
+		PasswordStrength result = meter.meter("ab12!@AB");
+		assertEquals(PasswordStrength.STRONG, result);
+		PasswordStrength result2 = meter.meter("abc1!Add");
+		assertEquals(PasswordStrength.STRONG, result2);
+	}
+
+}
