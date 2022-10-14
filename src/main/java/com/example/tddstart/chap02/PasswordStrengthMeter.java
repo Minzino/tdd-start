fe8ce3d348990318563c0e7c477d1ccda08c9e30
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-10-14 16:24:38 +0900

    :recycle: 메서드 리팩토링

--- src/main/java/com/example/tddstart/chap02/PasswordStrengthMeter.java
+++ src/main/java/com/example/tddstart/chap02/PasswordStrengthMeter.java
@@ -6,29 +6,23 @@ public class PasswordStrengthMeter {
 		if (s == null || s.isBlank()) {
 			return PasswordStrength.INVALID;
 		}
-		boolean lengthEnough = s.length() >= 8;
-		boolean containsNum = meetsContainingNumberCriteria(s);
-		boolean containsUpp = meentsContainingUppercaseCriteria(s);
 
-//		if (s.length() < 8) {
-//			return PasswordStrength.NORMAL;
-//		}
-		if (lengthEnough && !containsNum && !containsUpp) {
+		int metCounts = 0;
+
+		if (s.length() >= 8) {
+			metCounts++;
+		}
+		if (meetsContainingNumberCriteria(s)) {
+			metCounts++;
+		}
+		if (meetsContainingUppercaseCriteria(s)) {
+			metCounts++;
+		}
+
+		if (metCounts == 1) {
 			return PasswordStrength.WEAK;
 		}
-		if (!lengthEnough && containsNum && !containsUpp) {
-			return PasswordStrength.WEAK;
-		}
-		if (!lengthEnough && !containsNum && containsUpp) {
-			return PasswordStrength.WEAK;
-		}
-		if (!lengthEnough) {
-			return PasswordStrength.NORMAL;
-		}
-		if (!containsNum) {
-			return PasswordStrength.NORMAL;
-		}
-		if (!containsUpp) {
+		if (metCounts == 2) {
 			return PasswordStrength.NORMAL;
 		}
 		return PasswordStrength.STRONG;
@@ -43,7 +37,7 @@ public class PasswordStrengthMeter {
 		return false;
 	}
 
-	private boolean meentsContainingUppercaseCriteria(String s) {
+	private boolean meetsContainingUppercaseCriteria(String s) {
 		for (char ch : s.toCharArray()) {
 			if (Character.isUpperCase(ch)) {
 				return true;
