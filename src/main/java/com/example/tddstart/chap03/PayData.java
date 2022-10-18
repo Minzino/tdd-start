2bd7074603b3e71708ee6936fcb5271b148f2aa7
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-10-18 20:57:53 +0900

    :recycle: 여러 파라미터 대신 사용할 PayData 클래스

--- src/main/java/com/example/tddstart/chap03/PayData.java
+++ src/main/java/com/example/tddstart/chap03/PayData.java
@@ -4,13 +4,15 @@ import java.time.LocalDate;
 
 public class PayData {
 
+	private LocalDate firstBillingDate;
 	private LocalDate billingDate;
 	private int payAmount;
 
 	public PayData() {
 	}
 
-	public PayData(LocalDate billingDate, int payAmount) {
+	public PayData(LocalDate firstBillingDate, LocalDate billingDate, int payAmount) {
+		this.firstBillingDate = firstBillingDate;
 		this.billingDate = billingDate;
 		this.payAmount = payAmount;
 	}
@@ -31,6 +33,11 @@ public class PayData {
 
 		private PayData data = new PayData();
 
+
+		public Builder firstBillingDate(LocalDate firstBillingDate) {
+			data.firstBillingDate = firstBillingDate;
+			return this;
+		}
 		public Builder billingDate(LocalDate billingDate) {
 			data.billingDate = billingDate;
 			return this;
--- src/test/java/com/example/tddstart/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/com/example/tddstart/chap03/ExpiryDateCalculatorTest.java
@@ -35,6 +35,20 @@ class ExpiryDateCalculatorTest {
 			LocalDate.of(2020, 2, 29));
 	}
 
+	@Test
+	void 첫_납부일과_만료일_일자가_다를때_만원_납부() {
+		PayData payData = PayData.builder()
+			.firstBillingDate(LocalDate.of(2019, 1, 31))
+			.billingDate(LocalDate.of(2019, 2, 28))
+			.payAmount(10_000)
+			.build();
+		assertExpiryDate(PayData.builder()
+				.billingDate(LocalDate.of(2019, 3, 1))
+				.payAmount(10_000)
+				.build(),
+			LocalDate.of(2019, 4, 1));
+	}
+
 	private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
 		ExpiryDateCalculator cal = new ExpiryDateCalculator();
 		LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
