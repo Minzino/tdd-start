5244be0e76ebe3d9c78d4e8fd6727b4466265a6e
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-10-18 20:41:45 +0900

    :recycle: 변경된 파라미터 Paydata에 대해 리팩토링 - 빌더패턴 사용

--- src/main/java/com/example/tddstart/chap03/ExpiryDateCalculator.java
+++ src/main/java/com/example/tddstart/chap03/ExpiryDateCalculator.java
@@ -5,7 +5,14 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
-	public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-		return billingDate.plusMonths(1);
+//	public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+//		int cnt = 0;
+//		for (int i = 10000; i <= payAmount; i += 10000) {
+//			cnt++;
+//		}
+//		return billingDate.plusMonths(cnt);
+//	}
+	public LocalDate calculateExpiryDate(PayData payData) {
+		return payData.getBillingDate().plusMonths(1);
 	}
 }
